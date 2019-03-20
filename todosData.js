252736984cf290f28d0594600f5c91987316c1bb
Nishma <35844858+Maskey2@users.noreply.github.com>
2019-03-19 23:04:23 -0500

    Add files via upload

---
+++ todosData.js
@@ -0,0 +1,29 @@
+const todosData = [
+    {
+        id: 1,
+        text: "Practice React",
+        completed: true
+    },
+    {
+        id: 2,
+        text: "Grocery shopping",
+        completed: false
+    },
+    {
+        id: 3,
+        text: "Do Calculus Homework",
+        completed: false
+    },
+    {
+        id: 4,
+        text: "Get back to all Emails",
+        completed: true
+    },
+    {
+        id: 5,
+        text: "Keep all the builders busy in Clash of Clans",
+        completed: false
+    }
+]
+
+export default todosData
